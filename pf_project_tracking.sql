e11c8d286c3a8474f62be14d10597204b848516c
Ado Sibalo <asibalo@EPIC-MAC-SIBALO.local>
2019-09-19 13:55:48 -0500

    discovered true sources of expense data after much trial and error

--- pf_project_tracking.sql
+++ pf_project_tracking.sql
@@ -1,93 +1,98 @@
-SELECT pr.WBS1 AS ProjectNumber,
-	   REPLACE(pr.WBS2, ' ', '') AS PhaseName,
-	   SUM(pr.Fee) AS ApprovedPOAmount,
+SELECT PR.WBS1 AS ProjectNumber,
+	   REPLACE(PR.WBS2, ' ', '') AS PhaseName,
+	   SUM(PR.Fee) AS ApprovedPOAmount,
 	   expense.Amount AS MaterialsSpent,
 	   po_commit.Amount AS CommittedExpense,
 	   total_spent.Amount AS TotalSpent,
-	   CASE WHEN pr.RevType = 'TM' THEN pr.Fee
-	        ELSE ROUND((pr.Fee * pr.PctComp * 0.01), 2) 
+	   CASE WHEN PR.RevType = 'TM' THEN PR.Fee
+	        ELSE ROUND((PR.Fee * PR.PctComp * 0.01), 2) 
 	   END AS GAAPRevenue,
-	   pr.RevType AS BillingType,
-	   billed.Amount AS BilledInvoiceAmount,
-	   received.Amount AS ReceivedInvoiceAmount
-FROM [epicsysacct].[dbo].[PR] pr
+	   PR.RevType AS BillingType,
+	   invoiced.BilledAmount AS BilledInvoiceAmount,
+	   invoiced.ReceivedAmount AS ReceivedInvoiceAmount
+FROM PR
 LEFT JOIN
 (
 	SELECT WBS1,
-		   SUM(Amount) Amount
+	       WBS2,
+	       SUM(Amount) Amount
 	FROM 
 	(
-		 SELECT WBS1, 
-				Amount
-		 FROM   [epicsysacct].[dbo].[LedgerAP] -- Expense Detail Table - Accounts Payable
-		 UNION ALL
-		 SELECT WBS1,
-				Amount
-		 FROM   ekDetail -- Expense Table - Data Entry Detail
-		 UNION ALL
-		 SELECT WBS1, 
-				CostAmount AS Amount
-		 FROM   billExpDetail bill
-		 WHERE  NOT EXISTS
-			(
-				SELECT  NULL
-				FROM    LedgerAP lap
-				WHERE   lap.PKey = bill.OriginalPKey
-			)
-		 AND OriginalTable NOT IN ('LedgerAP', 'LedgerEX')
+		SELECT WBS1,
+		       WBS2, 
+			   Amount
+		FROM [epicsysacct].[dbo].[LedgerAP] -- Expense Detail Table - Accounts Payable
+		WHERE ProjectCost = 'Y' 
+		UNION ALL
+		SELECT WBS1,
+		       WBS2,
+			   Amount
+		FROM LedgerMisc -- Expense Table - Data Entry Detail
+		WHERE ProjectCost = 'Y' 
+		UNION ALL
+		SELECT WBS1,
+		       WBS2, 
+			   Amount
+		FROM   LedgerEX
+		WHERE ProjectCost = 'Y' 
+		UNION ALL
+		SELECT WBS1,
+		       WBS2, 
+			   Amount
+		FROM   LedgerAR 
+		WHERE ProjectCost = 'Y'
 	) MaterialExpense
-	GROUP BY WBS1
+	GROUP BY WBS1, WBS2
 ) expense
-ON expense.WBS1 = pr.WBS1
+ON expense.WBS1 = PR.WBS1 AND expense.WBS2 = PR.WBS2
 LEFT JOIN
 (
 	SELECT WBS1,
+	       WBS2,
 		   SUM(Amount) Amount
 	FROM 
 	(
-		 SELECT WBS1, 
-				Amount
-		 FROM   [epicsysacct].[dbo].[LedgerAP] -- Expense Detail Table - Accounts Payable
-		 UNION ALL
 		 SELECT WBS1,
-				Amount
-		 FROM   ekDetail -- Expense Table - Data Entry Detail
-		 UNION ALL
-		 SELECT WBS1,
+		        WBS2,
 				RegAmt+OvtAmt AS Amount
 		 FROM   LD -- Labor Detail Table
 		 UNION ALL
-		 SELECT WBS1, 
-				CostAmount AS Amount
-		 FROM   billExpDetail bill  -- Invoice Detail Table - Expense Detail for Prior Billings
-		 WHERE  NOT EXISTS
-			(
-				SELECT  NULL
-				FROM    LedgerAP lap
-				WHERE   lap.PKey = bill.OriginalPKey
-			)
-		 AND OriginalTable NOT IN ('LedgerAP', 'LedgerEX')
+		 SELECT WBS1,
+		        WBS2,
+				Amount
+		FROM [epicsysacct].[dbo].[LedgerAP] -- Expense Detail Table - Accounts Payable
+		WHERE ProjectCost = 'Y' 
+		UNION ALL
+		SELECT WBS1,
+		       WBS2,
+			   Amount
+		FROM LedgerMisc -- Expense Table - Data Entry Detail
+		WHERE ProjectCost = 'Y' 
+		UNION ALL
+		SELECT WBS1,
+		       WBS2,
+			   Amount
+		FROM   LedgerEX
+		WHERE ProjectCost = 'Y' 
+		UNION ALL
+		SELECT WBS1,
+		       WBS2,
+			   Amount
+		FROM   LedgerAR 
+		WHERE ProjectCost = 'Y'
 	) TotalSpent
-	GROUP BY WBS1
+	GROUP BY WBS1, WBS2
 ) total_spent
-ON total_spent.WBS1 = pr.WBS1
+ON total_spent.WBS1 = PR.WBS1 AND total_spent.WBS2 = PR.WBS2
 LEFT JOIN
 (
 	SELECT WBS1,
-		   SUM(Amount) AS Amount
-	FROM [epicsysacct].[dbo].[billINDetail]
+           ABS(SUM(CASE WHEN Account IN (401.00, 422.00, 421.00) THEN Amount END)) BilledAmount,
+	       ABS(SUM(CASE WHEN Account = 111.00 THEN Amount END)) ReceivedAmount
+	FROM LedgerAR
 	GROUP BY WBS1
-) billed
-ON billed.WBS1 = pr.WBS1
-LEFT JOIN
-(
-	SELECT WBS1,
-	       SUM(Amount) Amount
-	FROM [epicsysacct].[dbo].[crDetail]
-	WHERE Account = 111.00
-	GROUP BY WBS1
-) received
-ON received.WBS1 = pr.WBS1
+) invoiced
+ON invoiced.WBS1 = PR.WBS1
 LEFT JOIN
 (
 	SELECT WBS1,
@@ -96,18 +101,18 @@ LEFT JOIN
 	FROM POCommitment
 	GROUP BY WBS1
 ) po_commit
-ON po_commit.WBS1 = pr.WBS1
-WHERE pr.WBS1 IS NOT NULL AND TRY_CONVERT(INT, pr.WBS1) IS NULL 
-AND pr.WBS1+' ; '+pr.WBS2 IN {}
-GROUP BY pr.WBS1, 
+ON po_commit.WBS1 = PR.WBS1
+WHERE PR.WBS1 IS NOT NULL AND TRY_CONVERT(INT, PR.WBS1) IS NULL 
+AND PR.WBS1+' ; '+PR.WBS2 IN {}
+GROUP BY PR.WBS1, 
 		 RevType, 
-		 WBS2, 
-		 pr.PctComp,
-		 pr.Fee,
+		 PR.WBS2, 
+		 PR.PctComp,
+		 PR.Fee,
 		 expense.Amount,
 	     total_spent.Amount,
-	     billed.Amount,
-	     received.Amount,
+	     invoiced.ReceivedAmount,
+	     invoiced.BilledAmount,
 	     po_commit.Amount
-ORDER BY pr.WBS1
+ORDER BY PR.WBS1
 
